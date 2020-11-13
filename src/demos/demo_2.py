47445818041184a07de2b8e5e417df490d9fefcc
Tom Tarpey <tomtarpeydev@gmail.com>
2020-11-13 20:31:37 +0000

    added a print linked list and updated the reverse

--- src/demos/demo_2.py
+++ src/demos/demo_2.py
@@ -2,10 +2,13 @@
 Given a reference to the head node of a singly-linked list, write a function
 that reverses the linked list in place. The function should return the new head
 of the reversed list.
+
 In order to do this in O(1) space (in-place), you cannot make a new list, you
 need to use the existing nodes.
+
 In order to do this in O(n) time, you should only have to traverse the list
 once.
+
 *Note: If you get stuck, try drawing a picture of a small linked list and
 running your function by hand. Does it actually work? Also, don't forget to
 consider edge cases (like a list with only 1 or 0 elements).*
@@ -15,25 +18,57 @@ class LinkedListNode():
         self.value = value
         self.next  = None
 
-def reverse(head_of_list):
-    # Your code here
-    # create a ref to the head of list (our current node)
-    current_node = head_of_list
-    # create a pointer for the prev (set to null, None, nullptr)
-    prev = None
-    # create a pointer for a next (set to null, None, nullptr)
-    next = None
+def reverse(node):
+    """
+    we want the input node (the "head") to become the new tail 
+    we want the old tail to become the new head 
+    three pointers, prev_node, current_node, forward_node 
+    """
+    current_node = node 
+    prev_node = None 
+    forward_node = None 
 
-    # while the current node exists (traverse the linked list while the current node is not none) 
     while current_node:
-        # my next pointer gets set to the current nodes `next`
-        next = current_node.next
-        # my current nodes next gets set to the prev
-        current_node.next = prev
-        # set my prev pointer to my current node
-        prev = current_node
-        # set ref of current to next (increment to the next node in the chain)
-        current_node = next
+        # set `forward_node` to current node's `next`
+        forward_node = current_node.next 
 
-    # return the node that the prev pointer is currently pointing to
-    return prev
+        # switch the direction of `current`'s arrow 
+        current_node.next = prev_node 
+
+        # update our pointers
+        prev_node = current_node
+        current_node = forward_node
+
+    # we need to return the new head, which the `prev` pointer is referring to 
+    return prev_node
+
+def print_ll(node):
+    # set your `current_node` variable to the starting node 
+    current_node = node 
+
+    # keep moving `current_node` in a loop 
+    # while current_node is not none
+    while current_node is not None: 
+        # print the 'current_node' value
+        print(current_node.value)
+        # traverse the the next node
+        current_node = current_node.next 
+
+a = LinkedListNode('A')
+b = LinkedListNode('B')
+c = LinkedListNode('C')
+d = LinkedListNode('D')
+e = LinkedListNode('E')
+f = LinkedListNode('F')
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+e.next = f
+
+print_ll(a)
+print("---")
+new_head = reverse(a)
+
+print_ll(new_head)
